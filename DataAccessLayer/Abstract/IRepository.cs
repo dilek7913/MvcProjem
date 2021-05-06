4e020b87c4e1dfff23203ed7c2ea8f6bf56151a9
Dilek Uçmaz <81286450+dilek7913@users.noreply.github.com>
2021-05-06 12:21:32 +0300

    ProjeDosyaları

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T>
+    {
+        List<T> List();//listeleme
+        void Insert(T p);//ekleme
+        void Delete(T p);//sime
+        void Update(T p);//güncelleme
+        List<T> List(Expression<Func<T, bool>> filter);//şartlı listeleme
+    }
+}
